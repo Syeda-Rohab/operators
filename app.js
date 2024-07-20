875f47ab4024dc05871bcc21c74bd5b05424cb35
Syeda-Rohab <syedarohab8@gmail.com>
2024-07-20 09:38:09 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+"use strict";
+// operators ; 
+// + ) ;
+let num1 = 50;
+let num2 = 50;
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
---
+++ app.ts
@@ -0,0 +1,16 @@
+// operators ; 
+
+// + - * / ) ;
+
+// let num1 = 50 ;
+
+// let num2 = 50 ;
+
+// console.log ( num1 + num2 ) ;
+
+// console.log ( num1 - num2 ) ;
+
+// console.log ( num1 * num2 ) ;
+
+// console.log ( num1 / num2 ) ;
+
